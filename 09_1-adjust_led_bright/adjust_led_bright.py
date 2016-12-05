5f8e2800dac528e1514e783969f2b4d195d1f67a
sosorry <sosorry@raspberrypi.com.tw>
2016-12-06 01:01:54 +0800

    replace copyright/license headers from 2014 to 2016

--- 09_1-adjust_led_bright/adjust_led_bright.py
+++ 09_1-adjust_led_bright/adjust_led_bright.py
@@ -2,7 +2,7 @@
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 #|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# Copyright (c) 2014, raspberrypi.com.tw
+# Copyright (c) 2016, raspberrypi.com.tw
 # All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -20,7 +20,7 @@ GPIO.setmode(GPIO.BOARD)
 LED_PIN = 12
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-pwm_led = GPIO.PWM(LED_PIN, 50)
+pwm_led = GPIO.PWM(LED_PIN, 100)
 pwm_led.start(100)
 
 try:
@@ -28,7 +28,7 @@ try:
         duty_s = raw_input("Enter Brightness (0 to 100):")
         duty = int(duty_s)
 
-        if duty >= 0 and duty <=100 :
+        if duty >= 0 and duty <= 100 :
             pwm_led.ChangeDutyCycle(duty)
 
 except KeyboardInterrupt:
