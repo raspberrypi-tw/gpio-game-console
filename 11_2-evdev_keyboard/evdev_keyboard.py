74d432742938f09edf658f239c3ba2ca2131385c
sosorry <sosorry@raspberrypi.com.tw>
2020-03-20 21:57:07 +0800

    add time for emit event(from 0.05 to 1)

--- 11_2-evdev_keyboard/evdev_keyboard.py
+++ 11_2-evdev_keyboard/evdev_keyboard.py
@@ -4,7 +4,6 @@
 # Injecting input events. Display "hello" on the console
 #
 # http://python-evdev.readthedocs.org/en/latest/tutorial.html
-# -*- coding:utf-8 -*-
 
 import time
 from evdev import UInput, ecodes as e
@@ -13,19 +12,19 @@ ui = UInput()
 # accepts only KEY_* events by default
 ui.write(e.EV_KEY, e.KEY_H, 1)  # KEY_H down
 ui.write(e.EV_KEY, e.KEY_H, 0)  # KEY_H up
-time.sleep(0.05)
+time.sleep(0.1)
 ui.write(e.EV_KEY, e.KEY_E, 1)  
 ui.write(e.EV_KEY, e.KEY_E, 0)  
-time.sleep(0.05)
+time.sleep(0.1)
 ui.write(e.EV_KEY, e.KEY_L, 1) 
 ui.write(e.EV_KEY, e.KEY_L, 0)
-time.sleep(0.05)
+time.sleep(0.1)
 ui.write(e.EV_KEY, e.KEY_L, 1)
 ui.write(e.EV_KEY, e.KEY_L, 0) 
-time.sleep(0.05)
+time.sleep(0.1)
 ui.write(e.EV_KEY, e.KEY_O, 1)  
 ui.write(e.EV_KEY, e.KEY_O, 0) 
-time.sleep(0.05)
+time.sleep(0.1)
 ui.syn()
 ui.close()
 
